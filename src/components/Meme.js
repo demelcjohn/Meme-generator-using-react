bb1d3508c0aa497198bc2c066649e574f9993fae
demelcjohn <demal.john007@gmail.com>
2023-04-27 22:58:11 +0530

    nxt commit

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,16 @@
 import React from "react"
+import memesData from "../memesData.js"
 
 export default function Meme() {
+
+    let url
+    
+    function getMemeImage(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const {url} = memesArray[randomNumber].url
+         
+    }
     return (
         <main>
             <form className="form">
@@ -8,6 +18,7 @@ export default function Meme() {
                 <input type="text" placeholder="Bottom text" className="form--input" />
                 <button
                 className="form--button"
+                onClick={getMemeImage}
                 >Get a new meme image</button>
             </form>
         </main>
